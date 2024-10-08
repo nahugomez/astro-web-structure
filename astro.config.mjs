1a783ab4894f140005d86a00ea0d2a6950d609a4
Nahuel Gomez <nahuelgomezs@outlook.com>
2024-10-07 21:44:42 -0300

    Agrega Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
