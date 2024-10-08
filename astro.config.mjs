5b110e9d454ac26e811efbae66042fd3b470921d
Nahuel Gomez <nahuelgomezs@outlook.com>
2024-10-07 23:04:43 -0300

    Agrega Material Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import { mtConfig } from '@material-tailwind/react';
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}',
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [mtConfig],
+};
