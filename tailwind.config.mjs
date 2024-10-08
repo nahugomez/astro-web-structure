5b03815a1d6c44bb4030a1549cb8cb5ef5baef6b
Nahuel Gomez <nahuelgomezs@outlook.com>
2024-10-08 17:43:49 -0300

    Extiende las configuraciones de Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,28 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      // Centramos por defecto container, y agregamos padding por defecto en mobile
+      // https://tailwindcss.com/docs/container#centering-by-default
+      // https://tailwindcss.com/docs/container#adding-horizontal-padding
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
   },
 
-  plugins: [mtConfig],
+  plugins: [
+    mtConfig({
+      // https://www.material-tailwind.com/docs/v3/react/theming
+      // Objeto de configuracion de Material-Tailwind
+      // Aca cambiariamos colores, fonts y radius
+    }),
+  ],
 };
